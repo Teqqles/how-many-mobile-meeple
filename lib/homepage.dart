cd42d7dfaea1418d8f8b6ee993ef4f2c01dd8bdd
David Long <david.long@racing-leagues.co.uk>
2019-07-12 07:48:25 +0100

    Layout collections/geeklists in list and implement player count slider

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -21,37 +21,49 @@ class _MyHomePageState extends State<HomePage> {
         appBar: AppBar(
           title: Text('Geeklist & User Entry'),
         ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {},
+          icon: Icon(Icons.navigate_next),
+          label: Text("Random Game"),
+        ),
         body: Center(
             child: Column(children: <Widget>[
-          Container(
-            child: TextFormField(
-              controller: controller,
-              decoration: InputDecoration(
-                hintText: "bgg username/geeklist id",
-              ),
-            ),
-          ),
           Row(
             children: <Widget>[
+              SizedBox(width: 20),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.65,
+                child: TextFormField(
+                  controller: controller,
+                  decoration: InputDecoration(
+                    hintText: "bgg username/geeklist id",
+                  ),
+                ),
+              ),
+              SizedBox(width: 20),
               ScopedModelDescendant<AppModel>(
                 builder: (context, child, model) => RaisedButton(
-                  child: Text('Add User or Geeklist'),
+                  child: Text('Add'),
                   onPressed: () {
+                    if (controller.text.length == 0) return;
                     Item item = Item(controller.text);
                     model.addItem(item);
                     setState(() => controller.text = '');
                   },
                 ),
               ),
-//              Spacer(),
-//              RaisedButton(
-//                child: Text('Edit Users/Geeklists'),
-//                onPressed: () {
-//                  Navigator.of(context).push(MaterialPageRoute(
-//                      builder: (context) => RandomGameDisplayPage()));
-//                },
-//              )
             ],
+          ),
+          ScopedModelDescendant<AppModel>(
+            builder: (context, child, model) => Slider(
+              activeColor: Colors.indigoAccent,
+              min: 0.0,
+              max: 15.0,
+              onChanged: (newRating) {
+                setState(() => model.settings.playerCount = newRating.floor());
+              },
+              value: model.settings.playerCount.roundToDouble(),
+            ),
           ),
           ScopedModelDescendant<AppModel>(
             builder: (context, child, model) => Column(
@@ -59,13 +71,21 @@ class _MyHomePageState extends State<HomePage> {
                 context: context,
                 tiles: model.items.map(
                   (item) => ListTile(
-                    title: Text(item.name),
+                    title: Text(limitTitleLength(item.name)),
                     trailing: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: [
-                        Icon(Icons.delete, size: 30.0, color: Colors.red),
+                        Icon(Icons.person,
+                            size: 30.0,
+                            color: colorItem(item, ItemType.collection)),
+                        SizedBox(width: 20),
+                        Icon(Icons.format_list_bulleted,
+                            size: 30.0,
+                            color: colorItem(item, ItemType.geekList)),
+                        SizedBox(width: 20),
                         Icon(Icons.delete, size: 30.0, color: Colors.red),
                       ],
+                      mainAxisSize: MainAxisSize.min,
                     ),
                   ),
                 ),
@@ -73,5 +93,16 @@ class _MyHomePageState extends State<HomePage> {
             ),
           ),
         ])));
+  }
+
+  String limitTitleLength(String text) {
+    if (text.length >= 20) {
+      text = text.substring(0, 17) + "...";
+    }
+    return text;
+  }
+
+  Color colorItem(Item item, ItemType expectedType) {
+    return expectedType == item.itemType ? Colors.blue : Colors.blueGrey;
   }
 }
--- lib/model.dart
+++ lib/model.dart
@@ -3,6 +3,8 @@ import 'package:scoped_model/scoped_model.dart';
 class AppModel extends Model {
   List<Item> _items = [];
   List<Item> get items => _items;
+  Settings _settings = Settings(5);
+  Settings get settings => _settings;
 
   void addItem(Item item) {
     _items.add(item);
@@ -14,8 +16,26 @@ class AppModel extends Model {
   }
 }
 
+class ItemType {
+
+  static const collection = ItemType("collection");
+  static const geekList = ItemType("geeklist");
+
+  final String name;
+  const ItemType(this.name);
+}
+
+class Settings {
+  int playerCount;
+  Settings(this.playerCount);
+}
+
 class Item {
   final String name;
+  ItemType itemType;
 
-  Item(this.name);
+  Item(this.name) {
+    var isNumeric = this.name.contains(RegExp(r"^\d+$"));
+    this.itemType = isNumeric ? ItemType.geekList : ItemType.collection;
+  }
 }

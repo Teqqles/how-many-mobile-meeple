5ec7e6e783f5b1a3a11375b1fde7c8a40e6bf69f
David Long <david.long@racing-leagues.co.uk>
2019-07-12 17:22:13 +0100

    Add loading spinner to random game page

--- lib/randomgamedisplay.dart
+++ lib/randomgamedisplay.dart
@@ -1,7 +1,6 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 import 'homepage.dart';
@@ -9,8 +8,10 @@ import 'model.dart';
 
 class RandomGameDisplayPage extends StatelessWidget {
   static final String route = "Display-page";
+
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
         appBar: AppBar(
           title: Text('Users/GeekLists'),
@@ -18,8 +19,7 @@ class RandomGameDisplayPage extends StatelessWidget {
             RaisedButton(
               child: Text('Change Options'),
               onPressed: () {
-                Navigator
-                    .of(context)
+                Navigator.of(context)
                     .push(MaterialPageRoute(builder: (context) => HomePage()));
               },
             )
@@ -27,16 +27,38 @@ class RandomGameDisplayPage extends StatelessWidget {
         ),
         body: Container(
           child: ScopedModelDescendant<AppModel>(
-            builder: (context, child, model) => Column(
-                children: model.items
-                    .map((item) => ListTile(
-                  title: Text(item.name),
-                  onLongPress: () {
-                    model.deleteItem(item);
-                  },
-                ))
-                    .toList()),
-          ),
+              builder: (context, child, model) => Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: SpinKitCubeGrid(
+                                color: Theme.of(context).accentColor,
+                                size: screenWidth * 0.5
+                              ),
+                            ),
+                            Text("Finding games to play",
+                                style: TextStyle(fontWeight: FontWeight.bold)),
+                            Text(
+                              "this may take some time as board game geek is slow",
+                              style: TextStyle(fontSize: 12),
+                            )
+                          ])
+                    ],
+                  )
+//                Column(
+//                children: model.items
+//                    .map((item) => ListTile(
+//                  title: Text(item.name),
+//                  onLongPress: () {
+//                    model.deleteItem(item);
+//                  },
+//                ))
+//                    .toList()),
+              ),
         ));
   }
 }

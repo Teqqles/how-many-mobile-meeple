8f219ac896ea84494ea58855339fa55f5cca9709
David Long <david.long@racing-leagues.co.uk>
2019-07-15 16:05:07 +0100

    Extract form construction elements to improve readability

--- lib/game_config.dart
+++ lib/game_config.dart
@@ -1,5 +1,6 @@
 
 abstract class GameConfig {
+  static const int splashScreenDisplayTime = 3600;
   static const String optionsPageTitle = 'Game Options';
   static const String randomGamePageTitle = 'Random Game';
   static const int maxItemsFromBgg = 5;
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -29,149 +29,155 @@ class _MyHomePageState extends State<HomePage> with GameConfig, AppPage {
   }
 
   Future<void> hideScreen() async {
-    Future.delayed(Duration(milliseconds: 3600), () {
-      FlutterSplashScreen.hide();
-    });
+    Future.delayed(Duration(milliseconds: GameConfig.splashScreenDisplayTime),
+        () => FlutterSplashScreen.hide());
   }
 
   @override
   Widget build(BuildContext context) {
+    var textFieldWidth = MediaQuery.of(context).size.width * 0.65;
     return Scaffold(
         appBar: HowManyMeepleAppBar(GameConfig.optionsPageTitle),
         floatingActionButton: floatingRandomGameButton(context),
         body: Column(children: <Widget>[
-          Align(
-              alignment: Alignment.centerLeft,
-              child: AppDefaultPadding(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.65,
-                      child: ScopedModelDescendant<AppModel>(
-                        builder: (context, child, model) => TextFormField(
-                          enabled:
-                              model.items.length < GameConfig.maxItemsFromBgg,
-                          controller: controller,
-                          decoration: InputDecoration(
-                            hintText:
-                                model.items.length < GameConfig.maxItemsFromBgg
-                                    ? itemHintTextMessage
-                                    : maxItemsMessage,
-                          ),
-                        ),
-                      ),
-                    ),
-                    ScopedModelDescendant<AppModel>(
-                      builder: (context, child, model) => AppDefaultPadding(
-                        child: RaisedButton(
-                          child: Text('Add'),
-                          onPressed: () {
-                            if (controller.text.isEmpty) return;
-                            Item item = Item(controller.text);
-                            model.addItem(item);
-                            setState(() {
-                              controller.text = '';
-                            });
-                          },
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              )),
-          ScopedModelDescendant<AppModel>(
-            builder: (context, child, model) => Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                AppDefaultPadding(
-                  child: Text("Players?", textAlign: TextAlign.left),
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.60,
-                  child: Slider(
-                      activeColor: Theme.of(context).accentColor,
-                      min: 1.0,
-                      max: 10.0,
-                      divisions: 10,
-                      onChanged: (players) {
-                        setState(
-                            () => model.settings.playerCount = players.floor());
-                      },
-                      value: model.settings.playerCount.roundToDouble(),
-                      label:
-                          model.settings.playerCount.toString() + " players"),
-                ),
-                AppDefaultPadding(
-                  child: Container(
-                    decoration: ShapeDecoration(
-                        color: Theme.of(context).accentColor,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                        )),
-                    child: AppDefaultPadding(
-                      child: Text(model.settings.playerCount.toString(),
-                          textAlign: TextAlign.right,
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Theme.of(context).selectedRowColor)),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          ScopedModelDescendant<AppModel>(
-            builder: (context, child, model) => Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                AppDefaultPadding(
-                  child: Text("Time?", textAlign: TextAlign.left),
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.55,
-                  child: RangeSlider(
-                    activeColor: Theme.of(context).accentColor,
-                    min: 15.0,
-                    max: 300.0,
-                    divisions: 19,
-                    onChanged: (time) {
-                      setState(() {
-                        model.settings.minTime = time.start.floor();
-                        model.settings.maxTime = time.end.floor();
-                      });
-                    },
-                    values: RangeValues(model.settings.minTime.floorToDouble(),
-                        model.settings.maxTime.floorToDouble()),
-                    labels: RangeLabels(
-                        model.settings.minTime.toString() + " mins",
-                        model.settings.maxTime.toString() + " mins"),
-                  ),
-                ),
-                AppDefaultPadding(
-                  child: Container(
-                    decoration: ShapeDecoration(
-                        color: Theme.of(context).accentColor,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                        )),
-                    child: AppDefaultPadding(
-                      child: Text(
-                          model.settings.minTime.toString() +
-                              " - " +
-                              model.settings.maxTime.toString() +
-                              " mins",
-                          textAlign: TextAlign.right,
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Theme.of(context).selectedRowColor)),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
+          buildBoardGameItemTextField(textFieldWidth),
+          buildPlayerSliderDisplay(),
+          buildGameDurationSliderDisplay(),
           buildBoardGameGeekItemDisplay(),
         ]));
+  }
+
+  ScopedModelDescendant<AppModel> buildPlayerSliderDisplay() =>
+      ScopedModelDescendant<AppModel>(
+        builder: (context, child, model) => Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            AppDefaultPadding(
+              child: Text("Players?", textAlign: TextAlign.left),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.60,
+              child: Slider(
+                  activeColor: Theme.of(context).accentColor,
+                  min: 1.0,
+                  max: 10.0,
+                  divisions: 10,
+                  onChanged: (players) {
+                    setState(
+                        () => model.settings.playerCount = players.floor());
+                  },
+                  value: model.settings.playerCount.roundToDouble(),
+                  label: "${model.settings.playerCount.toString()} players"),
+            ),
+            AppDefaultPadding(
+              child: Container(
+                decoration: ShapeDecoration(
+                    color: Theme.of(context).accentColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    )),
+                child: AppDefaultPadding(
+                  child: Text(model.settings.playerCount.toString(),
+                      textAlign: TextAlign.right,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Theme.of(context).selectedRowColor)),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+
+  Widget buildBoardGameItemTextField(double textFieldWidth) => Align(
+      alignment: Alignment.centerLeft,
+      child: AppDefaultPadding(
+        child: Row(
+          children: <Widget>[
+            Container(
+              width: textFieldWidth,
+              child: ScopedModelDescendant<AppModel>(
+                builder: (context, child, model) => TextFormField(
+                  enabled: model.items.length < GameConfig.maxItemsFromBgg,
+                  controller: controller,
+                  decoration: InputDecoration(
+                    hintText: model.items.length < GameConfig.maxItemsFromBgg
+                        ? itemHintTextMessage
+                        : maxItemsMessage,
+                  ),
+                ),
+              ),
+            ),
+            ScopedModelDescendant<AppModel>(
+              builder: (context, child, model) => AppDefaultPadding(
+                child: RaisedButton(
+                  child: Text('Add'),
+                  onPressed: () {
+                    if (controller.text.isEmpty) return;
+                    Item item = Item(controller.text);
+                    model.addItem(item);
+                    setState(() {
+                      controller.text = '';
+                    });
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ));
+
+  ScopedModelDescendant<AppModel> buildGameDurationSliderDisplay() {
+    var sliderWidth = MediaQuery.of(context).size.width * 0.55;
+    var sliderMinValue = 15.0;
+    var sliderMaxValue = 300.0;
+    var sliderSteps = 19;
+    return ScopedModelDescendant<AppModel>(
+      builder: (context, child, model) => Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          AppDefaultPadding(
+            child: Text("Time?", textAlign: TextAlign.left),
+          ),
+          Container(
+            width: sliderWidth,
+            child: RangeSlider(
+              activeColor: Theme.of(context).accentColor,
+              min: sliderMinValue,
+              max: sliderMaxValue,
+              divisions: sliderSteps,
+              onChanged: (time) {
+                setState(() {
+                  model.settings.minTime = time.start.floor();
+                  model.settings.maxTime = time.end.floor();
+                });
+              },
+              values: RangeValues(model.settings.minTime.floorToDouble(),
+                  model.settings.maxTime.floorToDouble()),
+              labels: RangeLabels("${model.settings.minTime.toString()} mins",
+                  "${model.settings.maxTime.toString()} mins"),
+            ),
+          ),
+          AppDefaultPadding(
+            child: Container(
+              decoration: ShapeDecoration(
+                  color: Theme.of(context).accentColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  )),
+              child: AppDefaultPadding(
+                child: Text(
+                    "${model.settings.minTime.toString()} - ${model.settings.maxTime.toString()} mins",
+                    textAlign: TextAlign.right,
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).selectedRowColor)),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   ScopedModelDescendant<AppModel> buildBoardGameGeekItemDisplay() {
@@ -226,14 +232,13 @@ class _MyHomePageState extends State<HomePage> with GameConfig, AppPage {
 
   String limitTitleLength(String text) {
     if (text.length > 20) {
-      text = text.substring(0, 18) + "...";
+      text = "${text.substring(0, 18)}...";
     }
     return text;
   }
 
-  Color colorItem(Item item, ItemType expectedType) {
-    return expectedType == item.itemType
-        ? Theme.of(context).accentColor
-        : Theme.of(context).disabledColor;
-  }
+  Color colorItem(Item item, ItemType expectedType) =>
+      expectedType == item.itemType
+          ? Theme.of(context).accentColor
+          : Theme.of(context).disabledColor;
 }

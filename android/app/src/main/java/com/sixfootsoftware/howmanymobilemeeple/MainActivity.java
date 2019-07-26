ac77be33294fa09b2535bd4f4bec9c06d72ae59b
David Long <david.long@racing-leagues.co.uk>
2019-07-26 13:08:41 +0100

    Replace splash screen to fix Amazon Appstore bug

--- android/app/src/main/java/com/sixfootsoftware/howmanymobilemeeple/MainActivity.java
+++ android/app/src/main/java/com/sixfootsoftware/howmanymobilemeeple/MainActivity.java
@@ -4,12 +4,10 @@ import android.os.Bundle;
 
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
-import org.devio.flutter.splashscreen.SplashScreen;
 
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    SplashScreen.show(this, true);
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
   }

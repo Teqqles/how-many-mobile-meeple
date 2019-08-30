3adf2aeeba90d9e9af2bb27a4d25169b4d16c4c1
David Long <david.long@racing-leagues.co.uk>
2019-08-30 18:14:41 +0100

    Define setting drawer items and actions

--- lib/components/drawer_saved_setting.dart
+++ lib/components/drawer_saved_setting.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:how_many_mobile_meeple/model/app_preferences.dart';
+import 'package:how_many_mobile_meeple/model/model.dart';
+import 'package:how_many_mobile_meeple/storage/preferences_history.dart';
 
 class DrawerSavedSetting extends Container {
-  final String filterTitle;
-  final String id;
 
-  DrawerSavedSetting(this.filterTitle, this.id, BuildContext context)
+  static var iconSize = 30.0;
+
+  final String preferencesTitle;
+  final AppPreferences preferences;
+
+  DrawerSavedSetting(
+      this.preferencesTitle, this.preferences, BuildContext context)
       : super(
           padding: EdgeInsets.only(top: 12, bottom: 12, left: 8, right: 8),
           decoration: BoxDecoration(
@@ -15,31 +21,43 @@ class DrawerSavedSetting extends Container {
                   bottom: BorderSide(
                       width: 1, color: Theme.of(context).highlightColor))),
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               FlatButton(
+                  padding: EdgeInsets.zero,
                   child: Text(
-                    filterTitle,
+                    preferencesTitle,
                     textAlign: TextAlign.left,
                     style: TextStyle(
                         fontSize: 13, decoration: TextDecoration.underline),
                   ),
-                  onPressed: () {} // id},
-                  )
+                  onPressed: () {
+                    var model = AppModel.of(context);
+                    model.replaceItems(preferences.items);
+                    model.replaceSettings(preferences.settings);
+                    model.refreshState();
+                    Navigator.pop(context);
+                  },// id},
+                  ),
+              IconButton(
+                icon: Icon(
+                  Icons.delete,
+                  size: iconSize,
+                  color: Theme.of(context).errorColor,
+                ),
+                onPressed: () {
+                  var model = AppModel.of(context);
+                  var db = PreferencesHistoryDb();
+                  db.deletePreference(preferences.id);
+                  model.refreshState();
+                },
+              ),
             ],
           ),
         );
 
   static DrawerSavedSetting preferencesToDrawSettings(
       AppPreferences preferences, BuildContext context) {
-    return DrawerSavedSetting(preferences.id, preferences.title, context);
+    return DrawerSavedSetting(preferences.title, preferences, context);
   }
-
-  @override
-  bool operator ==(other) {
-
-  }
-
-  @override
-  int get hashCode => filterTitle.hashCode + id.hashCode;
 }

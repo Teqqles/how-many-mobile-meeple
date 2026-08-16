8c92dbc00e1e847e2e23a3927763221088ba8734
David Long <david.long@racing-leagues.co.uk>
2026-08-16 21:20:42 +0100

    feat: show top-mechanics chips above the mechanics filter
    Mount TopMechanicChipsWidget above MechanicFilterWidget on the homepage
    and in advanced mode.

--- lib/guided_flow/advanced_mode_widget.dart
+++ lib/guided_flow/advanced_mode_widget.dart
@@ -12,6 +12,7 @@ import 'package:how_many_mobile_meeple/components/player_filter_widget.dart';
 import 'package:how_many_mobile_meeple/components/rating_filter_widget.dart';
 import 'package:how_many_mobile_meeple/components/shelf_of_shame_filter_widget.dart';
 import 'package:how_many_mobile_meeple/components/time_filter_widget.dart';
+import 'package:how_many_mobile_meeple/components/top_mechanic_chips_widget.dart';
 
 class AdvancedModeWidget extends StatelessWidget {
   const AdvancedModeWidget({super.key});
@@ -35,6 +36,7 @@ class AdvancedModeWidget extends StatelessWidget {
         const SizedBox(height: 8),
         const RatingFilterWidget(),
         const SizedBox(height: 8),
+        const TopMechanicChipsWidget(),
         const MechanicFilterWidget(),
         const SizedBox(height: 8),
         const ShelfOfShameFilterWidget(),
